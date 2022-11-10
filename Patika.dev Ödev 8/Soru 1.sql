e8154214ecf17f6b7861ac89d429b1443a26fbd2
Hasan Emre AVARİSLİ <81103889+emreavarisli@users.noreply.github.com>
2022-11-10 22:47:56 +0300

    create and delete

---
+++ Patika.dev Ödev 8/Soru 1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE employee(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	birthday DATE,
+	email VARCHAR(100)
+);
---
+++ Patika.dev Ödev 8/Soru 3.sql
@@ -0,0 +1,26 @@
+UPDATE employee
+		 SET name = 'Emre ' ,
+		 email ='emre@gmail.com' ,
+		 birthday ='2002-04-18'
+		 WHERE id = 1 ;
+
+UPDATE employee
+		 SET name = 'Zeynep' ,
+		 email ='zeynep@gmail.com' ,
+		 birthday ='2003-10-23'
+		 WHERE id = 2 ;
+UPDATE employee
+		 SET name = 'Ahmet' ,
+		 email ='ahmet@gmail.com' ,
+		 birthday ='1995-04-25'
+		 WHERE id = 3 ;
+UPDATE employee
+		 SET name = 'Ozlem' ,
+		 email ='ozlem@gmail.com' ,
+		 birthday ='1998-07-23'
+		 WHERE id = 4 ;
+UPDATE employee
+		 SET name = 'Yunus ' ,
+		 email ='yunus@gmail.com' ,
+		 birthday ='2002-02-24'
+		 WHERE id = 5 ;
